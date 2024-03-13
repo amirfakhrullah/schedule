323f817c7143191f4c886007877f8651f45a3b75
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2024-03-14 01:12:58 +0800

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,10 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { setupRateLimitHeaders } from "./helpers/setupRateLimitHeaders";
-import { setupProdRateLimiter } from "./lib/ratelimit";
+import { NextRequest, NextResponse } from "next/server";
 
 const authPaths = ["/", "/auth", "/courses", "/notes", "search"];
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const ip = req.ip ?? "127.0.0.1";
-  const ratelimit = await setupProdRateLimiter(`mw_${ip}`);
-
-  const setupResponseHeaders = (res: NextResponse) =>
-    setupRateLimitHeaders(res, {
-      limit: ratelimit?.limit,
-      remaining: ratelimit?.remaining,
-      reset: ratelimit?.reset,
-    });
-
-  if (ratelimit) {
-    ev.waitUntil(ratelimit.pending);
-    // redirect to "/api/blocked" if exceed ratelimiter's limit
-    if (!ratelimit.success)
-      return setupResponseHeaders(
-        NextResponse.rewrite(new URL("/api/blocked", req.url), req)
-      );
-  }
-
+export async function middleware(req: NextRequest) {
   // early escape for routes that don't need auth checking
-  if (!authPaths.includes(req.nextUrl.pathname))
-    return setupResponseHeaders(NextResponse.next(req));
+  if (!authPaths.includes(req.nextUrl.pathname)) return NextResponse.next(req);
 
   const BASE_URL = req.nextUrl.origin;
 
@@ -38,13 +16,13 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   // if authed, redirect "/auth" route to "/"
   if (checkAuth.status === 200 && req.nextUrl.pathname === "/auth") {
-    return setupResponseHeaders(NextResponse.redirect(BASE_URL));
+    return NextResponse.redirect(BASE_URL);
   }
   // if not authed, redirect all routes to "/auth"
   if (checkAuth.status === 401 && req.nextUrl.pathname !== "/auth") {
-    return setupResponseHeaders(NextResponse.redirect(`${BASE_URL}/auth`));
+    return NextResponse.redirect(`${BASE_URL}/auth`);
   }
-  return setupResponseHeaders(NextResponse.next(req));
+  return NextResponse.next(req);
 }
 
 export const config = {
